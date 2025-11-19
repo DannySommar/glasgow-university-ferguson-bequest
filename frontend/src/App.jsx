81083224a892789e93337cda2c6201819392f1a5
dianapolese <diana.polese56@gmail.com>
2025-11-19 13:50:07 +0000

    installed react-router-dom as it was missed out in the previous commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { HashRouter as Router, Routes, Route} from 'react-router-dom'
 import { Attractions } from './pages/Attractions'
 import {Home} from './pages/Home'
 import { Layout } from './components/Layout'
-import 
 
 function App() {
   const [message, setMessage] = useState('')
