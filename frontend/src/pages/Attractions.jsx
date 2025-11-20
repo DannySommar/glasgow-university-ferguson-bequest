6fb86ac43c5ce542204a68f55a04fbc8265c3b57
dianapolese <diana.polese56@gmail.com>
2025-11-20 10:42:49 +0000

    Created an images folder to store the images for each attraction. Added styling css to the attractions page to look like the wireframe with an interactive design. Issue #25. Committing changes to a branch to be merged with main when approved

--- frontend/src/pages/Attractions.jsx
+++ frontend/src/pages/Attractions.jsx
@@ -1,19 +1,26 @@
 import { AttractionCard } from "../components/AttractionCard"
 import "./Attractions.css";
+import Blairimg from "../images/BlairDrumond.jpg"
+import zoo from "../images/EdZoo.jpg"
+import rsnoghost from "../images/Ghostbusters-Header.jpg"
+import clan from "../images/Clan.jpg"
 
 
 export function Attractions (){
 
-    const standcomedy = {title: "The Stand Comedy Club", desc: "Comedy club in Glasgow"}
-    const blairdrummond = {title: "Blair Drummond Safari Park", desc: "Safari park"}
-    const edizoo = {title: "Edinburgh Zoo", desc: "Zoo in Edinburgh"}
+    const blairdrummond = {title: "Blair Drummond Safari Park", img:Blairimg}
+    const edizoo = {title: "Edinburgh Zoo", img:zoo}
+    const hockey = {title: "Glasgow Clan Ice Hockey", img:clan}
+    const rsno = {title: "RSNO - Ghostbuster Concert",  img:rsnoghost}
 
-    const attractions = [standcomedy,blairdrummond, edizoo]
+
+    const attractions = [blairdrummond, edizoo, hockey, rsno]
 
     return (
         <>
         <h2>Attractions</h2>
-        <div className="attraction">
+        <div className="attractionPage">
+        
             
         {attractions.map((attractions)=>{
 
