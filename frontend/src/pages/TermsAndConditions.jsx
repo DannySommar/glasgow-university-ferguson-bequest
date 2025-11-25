7e1010af15f8ad87fc64f1bf691fcfacafc64037
Lewis Gray <2887454G@student.gla.ac.uk>
2025-11-25 22:39:33 +0000

    Added T&C popup and T&C page

---
+++ frontend/src/pages/TermsAndConditions.jsx
@@ -0,0 +1,31 @@
+import "./TermsAndConditions.css"
+
+export function TermsAndConditions() {
+    return (
+        <>
+        <h2>Terms & Condtions</h2>
+        <div className="TermsAndConditions">
+            <div className="Content">
+                <div className="Information">
+                    <h3>Before Staff apply for Attraction Tickets and ballots, they should be aware of the T&Cs:</h3>
+                    <p>Attraction and Ballot tickets are available to any member of staff of the University holding a contract of employment with the University. This does not include registered temporary / sessional staff or holders or individuals employed directly by other organisations but paid via the University payroll.</p>
+                    <p>Staff may apply for 3 attractions per calendar year and enter as many ballot entries as they wish - staff may win one ballot.</p>
+                    <p>Ballot winners will be removed from further ballots.</p>
+                    <p>Bookings are made on-line at MyGlasgow via the Ferguson Request.</p>
+                    <p>Instructions for booking will be emailed to you directly when you apply.</p>
+                    <p>Tickets and a ticket winner cannot be applied retrospectively, so please do not book attractions in advance without the instructions.</p>
+                    <p><strong>Please, please, do not apply if you are unable to attend</strong> - we want everyone to have a chance to obtain some tickets!</p>
+                    <p>If you believe you will not be able to use tickets, please contact fergusonrequest@glasgow.ac.uk immediately however, if these cannot be reallocated then it may affect further requests for tickets in the same calendar year.</p>
+                    <p>If, for one unforeseen circumstance, you are unable to attend a pre-booked attraction, you may not request further tickets for that attraction within the same year.</p>
+                    <p>It is your responsibility to ensure attractions and shows are suitable for all members of your party before booking.</p>
+                    <p>During visits, staff should be mindful that they are representing the University of Glasgow and ensure they, and members of their party, conduct themselves in a manner appropriate to the university and its values.</p>
+                    <p>Tickets are non transferable and should not be loaned to another person or another staff member.</p>
+                    <p>Tickets are equivalent to one entry and cannot be used as a substitute for monetary value towards goods and/or services.</p>
+                    <p>The University is in no way liable or responsible for other costs incurred during visits, re attraction cancellations or closures.</p>
+                    <p>Staff are required to present their staff card and confirmation email, as well as any entry tickets, on the day. Failure to do so will result in entry being denied or full charges applied.</p>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
