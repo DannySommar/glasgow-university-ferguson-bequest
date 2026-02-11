773ad209160f081334f28204db65d591eceb175b
Danny Shevchuk <2913985s@student.gla.ac.uk>
2026-02-11 12:40:19 +0000

    optimise: revies noe use user from authContext

--- frontend/src/pages/SpecificAttraction.jsx
+++ frontend/src/pages/SpecificAttraction.jsx
@@ -22,7 +22,7 @@ const imageMap = {
 }
 
 export function SpecificAttraction() {
-  const user = useAuth()
+  const { user }= useAuth()
   const { slug } = useParams()
   const [attraction, setAttraction] = useState(null)
   const [reviews, setReviews] = useState([])
@@ -79,7 +79,10 @@ export function SpecificAttraction() {
     })
 
     const savedReview = await response.json()
-    setReviews(prev => [savedReview, ...prev])
+    setReviews(prev => {
+      const filtered = prev.filter(r => r.user_id !== user.id) // removes prev user review right away
+      return [savedReview, ...filtered]
+    })
   }
 
   if (loading) return <p>loading attraction... </p>
@@ -110,7 +113,7 @@ export function SpecificAttraction() {
           
           {reviews.map(review => (
             <div key={review.id} className="singleReview">
-              <strong>{review.username || 'Anonymous'}</strong> {/* username from backend */}
+              <strong>{review.username}</strong> {/* add Anonymous later when we add the flag for it in reviews table */}
               <p>{review.comment}</p>
               <span>{review.rating} ★</span>
               <small>{new Date(review.created_at).toLocaleString()}</small>
