8225385f48acc52aadef6614cfb8556d70510181
Danny Shevchuk <2913985s@student.gla.ac.uk>
2026-03-04 11:56:04 +0000

    hopefully made it open to all hosts now, including on preview also

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.js'
-  },
-
   server: {
-    allowedHosts: true,
     host: true,
-
+    port: 5173
+  },
+  preview: {
+    host: true,
+    port: 5173
   }
-
 })
