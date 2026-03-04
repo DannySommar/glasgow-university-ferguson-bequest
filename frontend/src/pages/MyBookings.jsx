c2108f31351082aeae8370ab361ee7fc57eca7c9
Lewis Gray <2887454G@student.gla.ac.uk>
2026-03-04 15:06:47 +0000

    small change

--- frontend/src/pages/MyBookings.jsx
+++ frontend/src/pages/MyBookings.jsx
@@ -64,7 +64,7 @@ export function MyBookings(){
                     status: booking.status,
                     code: booking.code,
                     title: booking.title,
-                    img: imageMap[booking.img],
+                    img: booking.img,
                     description: booking.description,
                     location: booking.location
                 }))
